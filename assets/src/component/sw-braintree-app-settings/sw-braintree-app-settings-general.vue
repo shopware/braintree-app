a7177c6cfba343f44b8a684b35b94041b7f224cf
Michel Bade <m.bade@shopware.com>
2025-07-18 14:55:47 +0200

    fix: general tab inheritance

--- assets/src/component/sw-braintree-app-settings/sw-braintree-app-settings-general.vue
+++ assets/src/component/sw-braintree-app-settings/sw-braintree-app-settings-general.vue
@@ -4,28 +4,29 @@
     :title='$t("settings.general.title")'
     :is-loading='loading'
 >
-    <div class='sw-braintree-app-settings-general__threeDSecuredEnforced'>
+    <div class='content'>
         <mt-switch
-            class='sw-braintree-app-settings-general__threeDSecureEnforced__input'
+            class='sw-braintree-app-settings-general__threeDSecureEnforced'
             bordered
             :label='$t("settings.general.threeDSecureEnforced.label")'
-            :checked='activeConfig?.threeDSecureEnforced ?? undefined'
+            :checked='activeConfig.threeDSecureEnforced ?? rootConfig.threeDSecureEnforced ?? false'
+            :inherited-value='rootConfig.threeDSecureEnforced ?? false'
             :is-inherited='isFieldInherited("threeDSecureEnforced")'
             :is-inheritance-field='!!salesChannelId'
-            :help-text='$t("settings.general.threeDSecureToolTip")'
-            @change='activeConfig.threeDSecureEnforced = !activeConfig.threeDSecureEnforced'
-            @inheritance-remove='onRemove3DSInheritance()'
+            :help-text='$t("settings.general.threeDSecureEnforced.tooltip")'
+            @change='activeConfig.threeDSecureEnforced = $event'
+            @inheritance-remove='onRemoveInheritance("threeDSecureEnforced")'
             @inheritance-restore='onRestoreInheritance("threeDSecureEnforced")'
         />
-    </div>
-    <div class='sw-braintree-app-settings-general__shipsFromPostalCode'>
+
         <mt-text-field
-            class='sw-braintree-app-settings-general__shipsFromPostalCode__input'
+            class='sw-braintree-app-settings-general__shipsFromPostalCode'
             :label='$t("settings.general.shipsFromPostalCode.label")'
             :placeholder='$t("settings.general.shipsFromPostalCode.placeholder")'
+            :inherited-value='rootConfig.shipsFromPostalCode ?? ""'
             :is-inherited='isFieldInherited("shipsFromPostalCode")'
             :is-inheritance-field='!!salesChannelId'
-            :model-value='activeConfig.shipsFromPostalCode ?? ""'
+            :model-value='activeConfig.shipsFromPostalCode ?? rootConfig.shipsFromPostalCode ?? ""'
             @update:model-value='activeConfig.shipsFromPostalCode = $event'
             @inheritance-remove='onRemovePostalCodeInheritance()'
             @inheritance-restore='onRestoreInheritance("shipsFromPostalCode")'
@@ -77,44 +78,61 @@ export default defineComponent({
         },
 
         activeConfig(): ConfigEntity {
-            return this.config[this.stringifySalesChannelId] ?? DefaultConfigEntity(this.salesChannelId);
+            return this.config[this.stringifySalesChannelId] ?? {};
+        },
+
+        rootConfig(): ConfigEntity {
+            return this.config['null'] ?? {};
         },
     },
 
     watch: {
-        salesChannelId(){
-            void this.getConfig();
+        salesChannelId() {
+            if (!this.config[this.stringifySalesChannelId])
+                void this.getConfig(this.stringifySalesChannelId);
         },
     },
 
     created() {
-        void this.getConfig();
+        void this.getConfig('null');
         this.registerSaveHandler(this.updateConfig.bind(this));
     },
 
     methods: {
-        async getConfig(): Promise<void> {
-            if(this.config[this.stringifySalesChannelId]) return;
+        async getConfig(salesChannelId: string): Promise<void> {
+            this.loading = true;
 
-            return this.$api.get<ConfigEntity>('/entity/by-sales-channel/config/' + this.stringifySalesChannelId)
+            return this.$api.get<ConfigEntity>('/entity/by-sales-channel/config/' + salesChannelId)
                 .then((config) => {
-                    this.config[this.stringifySalesChannelId] = config;
-                    this.loading = false;
+                    this.config[String(config.salesChannelId)] = config;
+                    
+                    // add new config parameters
                     if (this.config['null']?.threeDSecureEnforced === null)
                         this.config['null'].threeDSecureEnforced = false;
 
+                    this.loading = false;
                 })
                 .catch((e) => this.$notify.error('fetch_settings', e));
         },
 
         async updateConfig(): Promise<void> {
-            return this.$api.patch<ConfigEntity>('/entity/config', Object.values(this.config))
-                .then(() => {
-                    this.config = {};
-                    void this.getConfig();
-                    this.$notify.success('save_settings');
-                })
-                .catch((e) => this.$notify.error('save_settings', e));
+            this.loading = true;
+            
+            try {
+                await this.$api.patch<ConfigEntity>('/entity/config', Object.values(this.config));
+                this.$notify.success('save_settings');
+
+                // reset, but preserve the active config to avoid flickering of toggles due to default values
+                this.config = {
+                    null: this.rootConfig,
+                    [this.stringifySalesChannelId]: this.activeConfig,
+                };
+
+                await Promise.all([this.getConfig('null'), this.getConfig(this.stringifySalesChannelId)]);
+            } catch (e) {
+                this.$notify.error('save_settings', e);
+                this.loading = false;
+            }
         },
 
         isFieldInherited(key: keyof ConfigEntity): boolean {
@@ -122,10 +140,6 @@ export default defineComponent({
                 return false;
 
             return this.activeConfig[key] === null;
-        },
-
-        onRemove3DSInheritance() {
-            this.activeConfig['threeDSecureEnforced'] = this.config['null']?.['threeDSecureEnforced'] ?? DefaultConfigEntity(this.salesChannelId)['threeDSecureEnforced'];
         },
 
         onRemovePostalCodeInheritance() {
@@ -147,11 +161,10 @@ export default defineComponent({
 
 <style lang='scss'>
 .sw-braintree-app-settings-general {
-    display: flex;
-    flex-direction: column;
-
-    &__shipsFromPostalCode {
-        margin-top: var(--scale-size-24);
+    .content {
+        display: flex;
+        flex-direction: column;
+        gap: var(--scale-size-24);
     }
 }
 </style>
